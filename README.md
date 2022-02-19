84af0edf1d75d932f2beaed5ccd4f09acd21da90
bvz <bvz@c381.com>
2022-02-19 02:05:56 -0800

    Updated to add a little bit of functionality.
    Added error checking
    Added a few options
    Added a usage string

--- README.md
+++ README.md
@@ -31,7 +31,8 @@ If you want to see any missing frames, you can use the -m option:
 test_name.1-3,5-9x2.jpg missing: [4, 6, 8]
 test2.1-3.jpg missing: []
 ```
+This tool requires that you first install bvzframespec (also available on github).
 
-This tool is very crude at the moment. The underlying library that it relies on (bvzframespec) is fairly robust, but this tool itself needs a fair bit of polish. For example, you must supply the directory (you cannot leave that portion blank). And if you use the -m option, it must come after the directory.
+This tool is relatively simple-minded at the moment. 
 
-These things will be improved in time. But for the moment, this tool does actually work.
+The underlying library that this tool relies on (bvzframespec) is fairly robust, but this tool itself could use a bit of polish. Even so, for the moment this tool does actually work well and given its minimal amount of needed utility, should do just fine.
--- bin/seq.py
+++ bin/seq.py
@@ -5,13 +5,49 @@ import sys
 
 from bvzframespec import Framespec
 
-files = os.listdir(sys.argv[1])
+if "-m" in sys.argv:
+    missing = True
+    sys.argv.remove("-m")
+else:
+    missing = False
+
+if "-h" in sys.argv:
+    hidden = True
+    sys.argv.remove("-h")
+else:
+    hidden = False
+
+if "-d" in sys.argv:
+    dirs = True
+    sys.argv.remove("-d")
+else:
+    dirs = False
+
+if len(sys.argv) > 2 or "-u" in sys.argv or "--help" in sys.argv or "--usage" in sys.argv:
+    print(f"Usage: {sys.argv[0]} <directory> <-m> <-h> <-d>")
+    print()
+    print("directory is optional. If omitted, the current working directory will be used.")
+    print("-m will show missing files from a sequence. Optional.")
+    print("-h will include hidden files. Optional.")
+    print("-d will include directories. Optional.")
+    sys.exit()
+
+if len(sys.argv) == 1:
+    directory = os.getcwd()
+else:
+    directory = sys.argv[1]
+
+files = os.listdir(directory)
 
 fs = Framespec()
 file_groups = fs.separate_list_into_lists_of_similar(files)
 for file_group in file_groups:
+    if not hidden and file_group[0].startswith("."):
+        continue
+    if not dirs and os.path.isdir(os.path.join(directory, file_group[0])):
+        continue
     fs.files_list = file_group
-    if "-m" in sys.argv:
+    if missing:
         print(fs.condensed_files_str, "missing:", fs.missing)
     else:
         print(fs.condensed_files_str)
